884caeaef71ee34c133ba1cab73d3fed90681bff
Niziol <niziol.luk@gmail.com>
2023-07-01 18:45:20 +0200

    adding header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import Header from './components/Layout/Header';
+
 function App() {
 	return (
-		<div>
-			<h2>Food Ordering App</h2>
-		</div>
+		<React.Fragment>
+			<Header />
+		</React.Fragment>
 	);
 }
 
