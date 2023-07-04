f0ad574a976e51acd032a1f70a90c30456f34d66
Niziol <niziol.luk@gmail.com>
2023-07-04 17:34:05 +0200

    working on adding items to cart and updating header value of total items in cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import { Fragment } from 'react';
 import mealsImg from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
 	return (
-		<React.Fragment>
+		<Fragment>
 			<header className={classes.header}>
 				<h1>ReactMeals</h1>
-				<HeaderCartButton />
+				<HeaderCartButton onClick={props.onShowCart} />
 			</header>
 			<div className={classes['main-image']}>
 				<img src={mealsImg} alt="A table full of delicious food!" />
 			</div>
-		</React.Fragment>
+		</Fragment>
 	);
 };
 
---
+++ src/store/CartProvider.js
@@ -0,0 +1,55 @@
+import { useReducer } from 'react';
+import CardContext from './cart-context';
+
+const defaultCartState = {
+	items: [],
+	totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+	switch (action.type) {
+		case 'ADD':
+			const updatedItems = state.items.concat(action.item);
+			const updatedTotalAmount =
+				state.totalAmount + action.item.price * action.item.amount;
+			return {
+				items: updatedItems,
+				totalAmount: updatedTotalAmount,
+			};
+
+		case 'REMOVE':
+			return;
+
+		default:
+			return defaultCartState;
+	}
+};
+
+const CartProvider = (props) => {
+	const [cartState, dispatchCartAction] = useReducer(
+		cartReducer,
+		defaultCartState
+	);
+
+	const addItemToCartHandler = (item) => {
+		dispatchCartAction({ type: 'ADD', item: item });
+	};
+	const removeItemFromCartHandler = (id) => {
+		dispatchCartAction({ type: 'REMOVE', id: id });
+	};
+
+	const cartContext = {
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
+		addItem: addItemToCartHandler,
+		removeItem: removeItemFromCartHandler,
+	};
+
+	return (
+		<CardContext.Provider value={cartContext}>
+			{props.children}
+		</CardContext.Provider>
+	);
+};
+
+export default CartProvider;
