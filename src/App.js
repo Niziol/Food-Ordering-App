d42c41354460dc180c1c5f51445ac5a7ceda6a86
Niziol <niziol.luk@gmail.com>
2023-07-04 16:37:55 +0200

    creating modal for cart items

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
 
 import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals'
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider'
 
 function App() {
+	const [cartIsShown, setCartIsShown] = useState(false);
+	const showCartHandle = () => {
+		setCartIsShown(true);
+	};
+
+	const hideCartHandle = () => {
+		setCartIsShown(false);
+	};
+
 	return (
-		<React.Fragment>
-			<Header />
+		<CartProvider>
+			{cartIsShown && <Cart onClose={hideCartHandle} />}
+			<Header onShowCart={showCartHandle} />
 			<main>
 				<Meals />
 			</main>
-		</React.Fragment>
+		</CartProvider>
 	);
 }
 
