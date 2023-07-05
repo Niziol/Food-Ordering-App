a8a54fe471793da5e7c3b18988aa4f4d737fab13
Niziol <niziol.luk@gmail.com>
2023-07-05 15:36:18 +0200

    adding and removing items from cart in cart modal

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,14 +8,14 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
 	switch (action.type) {
-		case 'ADD':
+		case 'ADD': {
 			const updatedTotalAmount =
 				state.totalAmount + action.item.price * action.item.amount;
 
-			const existingCardItemIndex = state.items.findIndex(
+			const existingCartItemIndex = state.items.findIndex(
 				(item) => item.id === action.item.id
 			);
-			const existingCartItem = state.items[existingCardItemIndex];
+			const existingCartItem = state.items[existingCartItemIndex];
 			let updatedItems;
 
 			if (existingCartItem) {
@@ -24,7 +24,7 @@ const cartReducer = (state, action) => {
 					amount: existingCartItem.amount + action.item.amount,
 				};
 				updatedItems = [...state.items];
-				updatedItems[existingCardItemIndex] = updatedItem;
+				updatedItems[existingCartItemIndex] = updatedItem;
 			} else {
 				updatedItems = state.items.concat(action.item);
 			}
@@ -33,9 +33,31 @@ const cartReducer = (state, action) => {
 				items: updatedItems,
 				totalAmount: updatedTotalAmount,
 			};
+		}
 
-		case 'REMOVE':
-			return;
+		case 'REMOVE': {
+			const existingCartItemIndex = state.items.findIndex(
+				(item) => item.id === action.id
+			);
+			const existingCartItem = state.items[existingCartItemIndex];
+			const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+			let updatedItems;
+			if (existingCartItem.amount === 1) {
+				updatedItems = state.items.filter((item) => item.id !== action.id);
+			} else {
+				const updatedItem = {
+					...existingCartItem,
+					amount: existingCartItem.amount - 1,
+				};
+				updatedItems = [...state.items];
+				updatedItems[existingCartItemIndex] = updatedItem;
+			}
+
+			return {
+				items: updatedItems,
+				totalAmount: updatedTotalAmount,
+			};
+		}
 
 		default:
 			return defaultCartState;
